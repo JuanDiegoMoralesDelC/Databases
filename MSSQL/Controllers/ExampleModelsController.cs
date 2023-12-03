d790956f88cd6347dd366e98b06b73b1733b9fcd
jdmdelc@gmail.com <jdmdelc@gmail.com>
2023-12-03 03:13:37 -0500

    added CRUD controller
    Fixed invariant culture problem

---
+++ MSSQL/Controllers/ExampleModelsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSSQL;
+using MSSQL.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MSSQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleModelsController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<ExampleModel> _entity;
+        public ExampleModelsController(ApplicationContext context)
+        {
+            _context = context;
+            _entity = _context.Set<ExampleModel>();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ExampleModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetExampleModels()
+        {
+            try
+            {
+                if (_entity == null) return NoContent();
+                var data = await _entity.ToListAsync();
+                return Ok(data);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetExampleModel(int id)
+        {
+            try
+            {
+                if (_entity == null) return NoContent();
+                var data = await _entity.FindAsync(id);
+
+                if (data == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(data);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> PutExampleModel(int id, ExampleModel data)
+        {
+            try
+            {
+                if (id != data.Id)
+                {
+                    return BadRequest();
+                }
+
+                _context.Entry(data).State = EntityState.Modified;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return !ExampleModelExists(id) ? NotFound() : StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ExampleModel>> PostExampleModel(ExampleModel data)
+        {
+            try
+            {
+                if (_entity == null) return NoContent();
+
+                _entity.Add(data);
+                await _context.SaveChangesAsync();
+
+                return Ok(data);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteExampleModel(int id)
+        {
+            try
+            {
+                if (_entity == null) return NoContent();
+
+                var data = await _context.ExampleModels.FindAsync(id);
+                if (data == null)
+                {
+                    return NotFound();
+                }
+
+                _entity.Remove(data);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        private bool ExampleModelExists(int id)
+        {
+            return _entity.Any(e => e.Id == id);
+        }
+    }
+}
