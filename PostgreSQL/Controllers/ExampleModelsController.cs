4696c2c62d11d911ac10158d77c1b90bc6653281
jdmdelc@gmail.com <jdmdelc@gmail.com>
2023-12-03 04:46:47 -0500

    Added PostgreSQL project

---
+++ PostgreSQL/Controllers/ExampleModelsController.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostgreSQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleModelsController : BaseController<ExampleModel>
+    {
+        public ExampleModelsController(ApplicationContext context) : base(context)
+        {
+
+        }
+    }
+}
